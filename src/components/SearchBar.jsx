5f12357959d0078a23cb188767c5b8bdd508c632
mkZ3R0 <bscs22021@itu.edu.pk>
2024-10-24 09:43:09 +0500

    Add search bar and state to main page + callback handler

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+
+const SearchBar = ({currentSearch, handleSearchClick}) => 
+{
+    const [searchTerm,setSearchTerm] = useState(currentSearch);
+
+    const handleSearchSubmit = (event) =>
+    {
+        event.preventDefault();
+
+        handleSearchClick(searchTerm);
+    };
+
+    const handleSearchInput = (event) =>
+    {
+        setSearchTerm(event.target.value);
+    };
+
+
+    return (
+        <div className="bg-gray-900 text-white">
+            <form className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7"
+            onSubmit={handleSearchSubmit}>
+                <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+                md:w-2/5">
+                    Every night <br></br><span className="text-red-700">is movie night</span>
+                </h1>
+                <div>
+                    <input type="text" placeholder="Looking for a specific film?" onChange={handleSearchInput}
+                    className="text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded 
+                    px-2 focus:outline-none" />
+                    <button className="bg-red-400 hover:bg-red-700 duration-300 px-5 py-2.5 font-[Poppins]
+                    rounded-md text-white md:w-auto w-full" type="submit" disabled={!searchTerm}>
+                    Find
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+
+};
+
+export default SearchBar;
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,13 +1,22 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import SearchBar from '../components/SearchBar'
+import {useState} from 'react'
 
 function Mainpage() {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchClick = (newSearch) =>
+  {
+    setSearchTerm(newSearch);
+  };
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-700">
       <Navbar />
-      {/* Add content here */}
-      <main class="mb-auto h-10 bg-grey-500">Content</main>
+      <SearchBar currentSearch={searchTerm} handleSearchClick={handleSearchClick} />
+      <main class="mb-auto h-10 bg-grey-500">{searchTerm}</main>
 
       <Footer className="mt-auto"/>
     </div>
