c10aa34c626cb66aa51af1ad81aa6779ec7bb240
mkZ3R0 <bscs22021@itu.edu.pk>
2024-10-24 09:00:31 +0500

    Initialize repository with tailwind config

---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,13 @@
+
+
+function Mainpage() {
+
+  return (
+    <div className="bg-black text-white">
+      <h1>Main Page</h1>
+      <p>This is the main page</p>
+    </div>
+  )
+}
+
+export default Mainpage
